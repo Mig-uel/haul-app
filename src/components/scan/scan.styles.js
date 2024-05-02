b7ec45313a6fe4876f49e983819e0bf7bcc40a1d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 19:41:55 -0400

    Add Scan component styles

---
+++ src/components/scan/scan.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ScannerContainer = styled.div`
+  margin-top: 50px;
+  text-align: center;
+  width: 100%;
+`
+
+export const CameraContainer = styled.div`
+  max-width: 500px;
+`
