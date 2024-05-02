41c871fc3d4ed2c6490250ae450811fe3c0a041e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 19:41:07 -0400

    Refactor App.jsx to add Scan component and update Hauls typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-
-import { AppContainer, HeaderContainer, Title } from './App.styles'
+import Scan from './components/scan/scan.component'
+import { AppContainer, HeaderContainer } from './App.styles'
 
 function App() {
   return (
     <AppContainer>
       <HeaderContainer>
-        <h1>Haul App</h1>
+        <h1>Haul</h1>
         <p>
           Track your recent food haul, save it to your Hauls, create future haul
           lists based on previous Hauls, and more coming.
         </p>
       </HeaderContainer>
+
+      <Scan />
     </AppContainer>
   )
 }
