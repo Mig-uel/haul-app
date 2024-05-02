ff41db4785b1de7637994bd2fb92bb3d31453089
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 19:41:24 -0400

    Add Haul component to project

---
+++ src/components/haul/haul.component.jsx
@@ -0,0 +1,12 @@
+import { HaulContainer } from './haul.styles'
+
+const Haul = () => {
+  return (
+    <HaulContainer>
+      <h1>[Home]</h1>
+      <p>[Most recent Haul shows up here]</p>
+    </HaulContainer>
+  )
+}
+
+export default Haul
