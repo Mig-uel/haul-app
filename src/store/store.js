d389e7ecadf7f6f74c3aafaadcdb3a31394aacdb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 20:26:05 -0400

    Add itemsReducer to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import itemsReducer from './items/itemsSlice.js'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    items: itemsReducer,
+  },
 })
