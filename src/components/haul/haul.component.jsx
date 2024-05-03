8ffee7fc33acf8586696c5600e1e93f29bc5b5c0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 21:51:18 -0400

    Update Haul component to display items from Redux store

--- src/components/haul/haul.component.jsx
+++ src/components/haul/haul.component.jsx
@@ -1,12 +1,31 @@
+import { useSelector } from 'react-redux'
+import { addItem } from '../../store/items/itemsSlice'
+
 import { HaulContainer } from './haul.styles'
+import HaulItems from '../haul-items/haul-items.component'
 
 const Haul = () => {
+  const items = useSelector(({ items }) => items)
+
   return (
-    <HaulContainer>
-      <h2>My Haul Items</h2>
-      {}
-    </HaulContainer>
+    <>
+      <h2 style={{ marginTop: '50px' }}>My Haul Items</h2>
+      <HaulContainer>
+        {items.map((item) => (
+          <HaulItems key={item} item={item} />
+        ))}
+      </HaulContainer>
+    </>
   )
 }
+
+// {scannedData &&
+//   items.map((item) => {
+//     return (
+//       <p key={item} onClick={() => dispatch(removeItem(item))}>
+//         {item}
+//       </p>
+//     )
+//   })}
 
 export default Haul
