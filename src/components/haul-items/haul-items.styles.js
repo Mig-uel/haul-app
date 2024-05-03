f0ce3dade3f1ea3b1b4e5f06ee8d86736b99bffe
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 21:51:29 -0400

    Add HaulItemsContainer styled component to haul-items.styles.js

---
+++ src/components/haul-items/haul-items.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HaulItemsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 400px;
+  width: 300px;
+
+  background-color: gray;
+
+  border-radius: 3px;
+`
