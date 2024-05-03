06b6f495749c4b328b33922f3f1a1e86f372a99b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-03 14:18:02 -0400

    Add apiSlice to handle API requests in the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemsReducer from './items/itemsSlice.js'
+import { apiSlice } from './api/apiSlice.js'
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
+    // [api]
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
