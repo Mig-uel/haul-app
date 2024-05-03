0432fa2cf5bebbc7e3ff63e64f444bb304aab3fa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-03 14:08:52 -0400

    Add apiSlice to handle API requests in the store

---
+++ src/store/api/apiSlice.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '',
+
+    endpoints: (builder) => ({
+      getItem: builder.query({
+        query: (id) => `https://us.openfoodfacts.org/api/v0/product/${id}.json`,
+      }),
+    }),
+  }),
+})
+
+export const { useGetItemQuery } = apiSlice
