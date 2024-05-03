9ec029c20d27ab855699eb8fedb1ac84484c4813
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-02 21:51:21 -0400

    Update Haul component styles in haul.styles.js

--- src/components/haul/haul.styles.js
+++ src/components/haul/haul.styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
 
 export const HaulContainer = styled.div`
-  margin-top: 50px;
+  margin-top: 40px;
+  margin-bottom: 50px;
   text-align: center;
-  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  max-width: 100%;
+
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  }
 `
