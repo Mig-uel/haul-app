5946574e7bc84760306f2e30ac561d9ca38da55b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-04 15:39:03 -0400

    Update haul-items.component.jsx to fetch item data and display it in the component

--- src/components/haul-items/haul-items.component.jsx
+++ src/components/haul-items/haul-items.component.jsx
@@ -1,14 +1,32 @@
 import { removeItem } from '../../store/items/itemsSlice'
 import { useDispatch } from 'react-redux'
+import { useGetItemQuery } from '../../store/api/apiSlice.js'
+import BarLoader from 'react-spinners/BarLoader.js'
 
-import { HaulItemsContainer } from './haul-items.styles'
+import { FaTrash } from 'react-icons/fa'
+import {
+  HaulItemInfoContainer,
+  HaulItemContainer,
+  HaulItemTitle,
+} from './haul-items.styles'
 
 const HaulItems = ({ item }) => {
   const dispatch = useDispatch()
+  const { data, isLoading, isSuccess, isError, error } = useGetItemQuery(item)
+  data && console.dir(data)
+
   return (
-    <HaulItemsContainer onClick={() => dispatch(removeItem(item))}>
-      <h5>{item}</h5>
-    </HaulItemsContainer>
+    <HaulItemContainer>
+      {isLoading ? (
+        <BarLoader />
+      ) : (
+        <HaulItemInfoContainer>
+          <HaulItemTitle>{data.product.product_name}</HaulItemTitle>
+          <p>{data.product.code}</p>
+          <FaTrash onClick={() => dispatch(removeItem(item))} />
+        </HaulItemInfoContainer>
+      )}
+    </HaulItemContainer>
   )
 }
 
