11d1791c97f66361caaca669d03a5a3d8608e54b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-04 15:39:08 -0400

    Refactor haul-items.styles.js to update container name and add new styles for item info

--- src/components/haul-items/haul-items.styles.js
+++ src/components/haul-items/haul-items.styles.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 
-export const HaulItemsContainer = styled.div`
+export const HaulItemContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 400px;
   width: 300px;
-
   background-color: gray;
-
   border-radius: 3px;
 `
+export const HaulItemInfoContainer = styled.div`
+  background-color: white;
+  opacity: 0.5;
+`
+export const HaulItemTitle = styled.h5``
