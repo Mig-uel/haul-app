c380ac174e8f67c10f1be327e6da54fc3290a7eb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-04 15:52:57 -0400

    Add haulsReducer to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemsReducer from './items/itemsSlice.js'
+import haulsReducer from './hauls/haulsSlice.js'
 import { apiSlice } from './api/apiSlice.js'
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
+    hauls: haulsReducer,
     // [api]
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
