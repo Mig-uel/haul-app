6dbc8fd0ea299510e3ae9274e4a81a1db274f69c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-05 14:35:03 -0400

    Refactor store.js to add loadingReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemsReducer from './items/itemsSlice.js'
 import haulsReducer from './hauls/haulsSlice.js'
+import loadingReducer from './loading/loadingSlice.js'
 import { apiSlice } from './api/apiSlice.js'
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
     hauls: haulsReducer,
+    loading: loadingReducer,
     // [api]
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
