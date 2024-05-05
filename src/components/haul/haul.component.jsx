f7b05692c98b1219547e12fe75c150456b690d8d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-05-05 14:34:45 -0400

    Optimize rendering of haul items in haul.component.jsx

--- src/components/haul/haul.component.jsx
+++ src/components/haul/haul.component.jsx
@@ -1,20 +1,20 @@
 import { useSelector } from 'react-redux'
+import HaulItems from '../haul-items/haul-items.component'
 
 import { HaulContainer } from './haul.styles'
-import HaulItems from '../haul-items/haul-items.component'
 
 const Haul = () => {
   const items = useSelector(({ items }) => items)
-  console.log(items)
 
   return (
     <>
       {items.length > 0 && <h2 style={{ marginTop: '50px' }}>Scanned Items</h2>}
       <HaulContainer>
         {items.map((item) => (
-          <HaulItems key={item.id} item={item.id} />
+          <HaulItems key={item} item={item} />
         ))}
       </HaulContainer>
+      {items.length > 0 && <button>Create Haul</button>}
     </>
   )
 }
